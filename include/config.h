0b08b6a1f8887c20bd7cff98a0b51c78a9a12144
Lukas Tobler <luk4s.tobler@gmail.com>
2017-02-19 17:42:26 +0100

    Add more input configuration options

--- include/config.h
+++ include/config.h
@@ -64,9 +64,16 @@ struct wavy_config_t {
 
 struct input_config {
     const char *name;
-    int tap_state;
-    int scroll_method;
-    int accel_profile;
+
+    int     tap_state;
+    int     scroll_method;
+    int     accel_profile;
+    int     tap_and_drag;
+    int     tap_and_drag_lock;
+    int     disable_while_typing;
+    int     middle_emulation;
+    int     click_method;
+    double  accel_speed; // range: [-1, 1]
 };
 
 extern char *cmdline_config_file;
--- src/config.c
+++ src/config.c
@@ -309,7 +309,7 @@ static void set_input_config(lua_State *L, int32_t idx,
                 luaL_error(L, "Invalid option for \'tap_to_click\': %s", value);
             }
         } else if (!strcmp(key, "scroll_method")) {
-            if (!strcmp(value, "no scroll")) {
+            if (!strcmp(value, "no_scroll")) {
                 ic->scroll_method = LIBINPUT_CONFIG_SCROLL_NO_SCROLL;
             } else if (!strcmp(value, "twofinger")) {
                 ic->scroll_method = LIBINPUT_CONFIG_SCROLL_2FG;
@@ -330,11 +330,113 @@ static void set_input_config(lua_State *L, int32_t idx,
                 luaL_error(L, "Invalid option for \'acceleration_profile\': %s",
                         value);
             }
+        } else if (!strcmp(key, "acceleration_speed")) {
+            double speed = (double) lua_tonumber(L, -1);
+            if (!(speed >= -1.0 && speed <= 1.0)) {
+                luaL_error(L, "Invalid option for \'acceleration_speed\': %f "
+                            "(must be in range [-1, 1])", speed);
+            }
+            ic->accel_speed = speed;
+        } else if (!strcmp(key, "tap_and_drag")) {
+            if (!strcmp(value, "enabled")) {
+                ic->tap_and_drag = LIBINPUT_CONFIG_DRAG_ENABLED;
+            } else if (!strcmp(value, "disabled")) {
+                ic->tap_and_drag = LIBINPUT_CONFIG_DRAG_DISABLED;
+            } else {
+                luaL_error(L, "Invalid option for \'tap_and_drag\': %s", value);
+            }
+        } else if (!strcmp(key, "tap_and_drag_lock")) {
+            if (!strcmp(value, "enabled")) {
+                ic->tap_and_drag_lock = LIBINPUT_CONFIG_DRAG_LOCK_ENABLED;
+            } else if (!strcmp(value, "disabled")) {
+                ic->tap_and_drag_lock = LIBINPUT_CONFIG_DRAG_LOCK_DISABLED;
+            } else {
+                luaL_error(L, "Invalid option for \'tap_and_drag_lock\': %s",
+                        value);
+            }
+        } else if (!strcmp(key, "disable_while_typing")) {
+            if (!strcmp(value, "enabled")) {
+                ic->disable_while_typing = LIBINPUT_CONFIG_DWT_ENABLED;
+            } else if (!strcmp(value, "disabled")) {
+                ic->disable_while_typing = LIBINPUT_CONFIG_DWT_DISABLED;
+            } else {
+                luaL_error(L, "Invalid option for \'disable_while_typing\': %s",
+                        value);
+            }
+        } else if (!strcmp(key, "middle_emulation")) {
+            if (!strcmp(value, "enabled")) {
+                ic->middle_emulation = LIBINPUT_CONFIG_MIDDLE_EMULATION_ENABLED;
+            } else if (!strcmp(value, "disabled")) {
+                ic->middle_emulation
+                    = LIBINPUT_CONFIG_MIDDLE_EMULATION_DISABLED;
+            } else {
+                luaL_error(L, "Invalid option for \'middle_emulation\': %s",
+                        value);
+            }
+        } else if (!strcmp(key, "click_method")) {
+            if (!strcmp(value, "clickfinger")) {
+                ic->click_method = LIBINPUT_CONFIG_CLICK_METHOD_CLICKFINGER;
+            } else if (!strcmp(value, "button_areas")) {
+                ic->click_method = LIBINPUT_CONFIG_CLICK_METHOD_BUTTON_AREAS;
+            } else if (!strcmp(value, "none")) {
+                ic->click_method = LIBINPUT_CONFIG_CLICK_METHOD_NONE;
+            }
         }
 
         lua_pop(L, 1);
     }
     lua_settop(L, idx);
+}
+
+static void apply_input_config(struct input_config *ic,
+        struct libinput_device *device) {
+
+    if (ic->tap_state != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set tap_enabled(%d) for \'%s\'",
+                ic->tap_state, ic->name);
+        libinput_device_config_tap_set_enabled(device, ic->tap_state);
+    }
+    if (ic->scroll_method != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set scroll_method(%d) for \'%s\'",
+                ic->scroll_method, ic->name);
+        libinput_device_config_scroll_set_method(device, ic->scroll_method);
+    }
+    if (ic->accel_profile != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set accel_profile(%d) for \'%s\'",
+                ic->accel_profile, ic->name);
+        libinput_device_config_accel_set_profile(device, ic->accel_profile);
+    }
+    if (ic->tap_and_drag != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set tap_and_drag(%d) for \'%s\'",
+                ic->tap_and_drag, ic->name);
+        libinput_device_config_accel_set_profile(device, ic->tap_and_drag);
+    }
+    if (ic->tap_and_drag_lock != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set tap_and_drag_lock(%d) for \'%s\'",
+                ic->tap_and_drag_lock, ic->name);
+        libinput_device_config_accel_set_profile(device, ic->tap_and_drag_lock);
+    }
+    if (ic->disable_while_typing != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set disable_while_typing(%d) for \'%s\'",
+                ic->disable_while_typing, ic->name);
+        libinput_device_config_accel_set_profile(device,
+                ic->disable_while_typing);
+    }
+    if (ic->middle_emulation != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set middle_emulation(%d) for \'%s\'",
+                ic->middle_emulation, ic->name);
+        libinput_device_config_accel_set_profile(device, ic->middle_emulation);
+    }
+    if (ic->click_method != INT_MIN) {
+        wavy_log(LOG_DEBUG, "Set click_method(%d) for \'%s\'",
+                ic->click_method, ic->name);
+        libinput_device_config_accel_set_profile(device, ic->click_method);
+    }
+    if (!isnan(ic->accel_speed)) {
+        wavy_log(LOG_DEBUG, "Set accel_speed(%f) for \'%s\'",
+                ic->accel_speed, ic->name);
+        libinput_device_config_accel_set_speed(device, ic->accel_speed);
+    }
 }
 
 static void input_configs_init(lua_State *L) {
@@ -367,6 +469,12 @@ static void input_configs_init(lua_State *L) {
         ic->tap_state = INT_MIN;
         ic->scroll_method = INT_MIN;
         ic->accel_profile = INT_MIN;
+        ic->tap_and_drag = INT_MIN;
+        ic->tap_and_drag_lock = INT_MIN;
+        ic->disable_while_typing = INT_MIN;
+        ic->middle_emulation = INT_MIN;
+        ic->click_method = INT_MIN;
+        ic->accel_speed = NAN;
 
         int32_t conf_table = lua_gettop(L);
         set_input_config(L, conf_table, ic);
@@ -439,26 +547,6 @@ void free_config() {
     vector_foreach(config->input_configs, free_input_config);
     vector_free(config->input_configs);
     free(config);
-}
-
-static void apply_input_config(struct input_config *ic,
-        struct libinput_device *device) {
-
-    if (ic->tap_state != INT_MIN) {
-        wavy_log(LOG_DEBUG, "Set tap_enabled(%d) for \'%s\'",
-                ic->tap_state, ic->name);
-        libinput_device_config_tap_set_enabled(device, ic->tap_state);
-    }
-    if (ic->scroll_method != INT_MIN) {
-        wavy_log(LOG_DEBUG, "Set scroll_method(%d) for \'%s\'",
-                ic->scroll_method, ic->name);
-        libinput_device_config_scroll_set_method(device, ic->scroll_method);
-    }
-    if (ic->accel_profile != INT_MIN) {
-        wavy_log(LOG_DEBUG, "Set accel_profile(%d) for \'%s\'",
-                ic->accel_profile, ic->name);
-        libinput_device_config_accel_set_profile(device, ic->accel_profile);
-    }
 }
 
 void configure_input(struct libinput_device *device) {
